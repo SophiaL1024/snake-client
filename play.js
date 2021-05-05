652f35a3e5732e2cd773dad5f42f8e1f5852bee9
SophiaL1024 <sophiafengliu@gmail.com>
2021-05-05 21:15:22 +0000

    creat input module

--- play.js
+++ play.js
@@ -1,49 +1,38 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
-const conn = connect();
-conn.on('data', (data) => {
+connect();
+setupInput();
+connect().on('data', (data) => {
   console.log(data);
 })
-conn.on('connect', () => {
+connect().on('connect', () => {
   console.log("Successfully connected to game server");
-  conn.write("Name: FL");
+  connect().write("Name: FL");
 
   setTimeout(() => {
-    conn.write("Move: up")
+    connect().write("Move: up")
   },
     1000);
   setTimeout(() => {
-    conn.write("Move: left")
+    connect().write("Move: left")
   },
     2000);
   setTimeout(() => {
-    conn.write("Move: down")
+    connect().write("Move: down")
   },
     3000);
   setTimeout(() => {
-    conn.write("Move: right")
+    connect().write("Move: right")
   },
     4000);
   const timerId = setInterval(() => {
-    conn.write("Move: up")
+    connect().write("Move: up")
   }, 500);
   setTimeout(() => {
     clearInterval(timerId);
   }, 5000);
 })
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-const handleUserInput = function (data) {
-  // your code here
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-setupInput();
+
 setupInput().on("data", handleUserInput);
